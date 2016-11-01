7552760bb3475356eec89ede492fdd553cab5ee8
ben <ben.loh.15@ucl.ac.uk>
2016-11-01 17:43:49 +0000

    Add some comments about usage to python wrapper

--- instance-starter-script/start_instance_py_wrapper.py
+++ instance-starter-script/start_instance_py_wrapper.py
@@ -1,6 +1,7 @@
 import subprocess
 import json
 
+# Change --key-name, --security-group-ids as needed for the AWS account
 start_cmd = "aws ec2 run-instances --image-id ami-5ec1673e --count 1" + \
 	" --instance-type t2.micro --key-name server1keypair" + \
 	" --security-group-ids sg-156c1b6c --user-data file://init_instance.sh"
@@ -11,6 +12,7 @@ output = json.loads(output.decode("utf-8"))
 
 instanceID = output['Instances'][0]['InstanceId']
 
+# Change the following to change the name or other tags
 tags = 'Key=Name,Value=Server1'
 label_cmd = 'aws ec2 create-tags --resources ' + instanceID + \
 	' --tags ' + tags
