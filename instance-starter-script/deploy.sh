84427c22f776c1d56b96e1fcd4ff61eaea09d250
now-raymond <rayjta@gmail.com>
2016-11-03 12:46:08 +0000

    update links

--- instance-starter-script/deploy.sh
+++ instance-starter-script/deploy.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
+# Removes existing files if present then downloads latest application files from Github repo.
+
 cd /home/ec2-user
+rm -rf strange-references*
 curl -L -u blzq-mu:3669b531d5d5ae756280723fd071e0a1640db581 \
-https://github.com/ryantantiern/strange-references/archive/hooklistener.zip \
+https://github.com/ryantantiern/strange-references/archive/master.zip \
 > strange-references.zip
 unzip strange-references.zip
 rm strange-references.zip
-mv strange-references-hooklistener/ strange-references/
+mv strange-references-master/ strange-references/
 
 chmod 755 `find . -type d`
 chmod 644 `find . -type f`
